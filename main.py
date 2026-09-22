82d85c5873b5153165379f59845ac1905113af87
trooler1 <trwlr0435@gmail.com>
2026-09-22 05:26:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,10 @@ TOKEN = os.getenv("BOT_TOKEN", "")
 CHANNEL_GUID = os.getenv("CHANNEL_GUID", "")
 PORT = int(os.getenv("PORT", "10000"))
 
-if not TOKEN:
-raise ValueError("BOT_TOKEN is not set")
-
-if not CHANNEL_GUID:
-raise ValueError("CHANNEL_GUID is not set")
-
 server = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
 threading.Thread(target=server.serve_forever, daemon=True).start()
 
-print("WEB SERVER STARTED ON PORT", PORT)
+print("WEB SERVER STARTED:", PORT)
 print("CHANNEL:", CHANNEL_GUID)
 
 bot = Robot(token=TOKEN)
