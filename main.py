ee031b88080b70f204848610457ed0a308435f7b
trooler1 <trwlr0435@gmail.com>
2026-09-22 11:42:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import os
 import threading
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-
 from rubka import Robot
 
 PORT = int(os.getenv("PORT", 10000))
 
 TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_GUID = os.getenv("CHANNEL_GUID")
 
-
-# جلوگیری از Sleep رندر
 server = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
+
 threading.Thread(
     target=server.serve_forever,
     daemon=True
@@ -21,46 +18,22 @@ threading.Thread(
 bot = Robot(token=TOKEN)
 
 
-async def is_member(user_id):
+@bot.on_message()
+def test(bot, message):
+
+    print("========== NEW MESSAGE ==========")
+    print("CHAT ID:", message.chat_id)
 
     try:
-        members = await bot.get_channel_members(CHANNEL_GUID)
-
-        for member in members:
-            if member.get("user_guid") == user_id:
-                return True
-
-        return False
+        print("FULL MESSAGE:")
+        print(message)
 
     except Exception as e:
-        print("MEMBER CHECK ERROR:", e)
-        return False
+        print("ERROR:", e)
 
-
-
-@bot.on_message()
-async def main(bot, message):
-
-    user_id = message.chat_id
-
-    print("USER:", user_id)
-
-    status = await is_member(user_id)
-
-    print("MEMBER:", status)
-
-
-    if status:
-        await message.reply(
-            "✅ عضویت شما تایید شد"
-        )
-
-    else:
-        await message.reply(
-            "❌ هنوز عضو کانال نیستید\n\n"
-            "ابتدا عضو شوید:\n"
-            "https://rubika.ir/AMIRTROOLER"
-        )
+    message.reply(
+        "آیدی دریافت شد:\n" + str(message.chat_id)
+    )
 
 
 print("BOT STARTED")
