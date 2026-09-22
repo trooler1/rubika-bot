d0918ac440898c549c67c02b513ba1024033d9a1
trooler1 <trwlr0435@gmail.com>
2026-09-22 05:30:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import threading
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-from rubka import Robot
+from rubka import Robot, Message
 
 TOKEN = os.getenv("BOT_TOKEN", "")
 CHANNEL_GUID = os.getenv("CHANNEL_GUID", "")
@@ -14,6 +14,13 @@ print("WEB SERVER STARTED:", PORT)
 print("CHANNEL:", CHANNEL_GUID)
 
 bot = Robot(token=TOKEN)
+
+@bot.on_message(commands=["start"])
+def start(bot, message):
+if bot.check_join(CHANNEL_GUID, message.chat_id):
+message.reply("✅ شما عضو کانال هستید!\n\nخوش آمدید 🌹")
+else:
+message.reply("❌ شما هنوز عضو کانال نیستید.\n\nابتدا در کانال عضو شوید و سپس دوباره /start را بزنید.")
 
 print("BOT STARTED")
 
