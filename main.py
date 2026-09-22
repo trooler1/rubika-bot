705a01e88b01906a44ee6dde07ac66f20d4e5036
trooler1 <trwlr0435@gmail.com>
2026-09-22 05:35:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ CHANNEL_GUID = os.getenv("CHANNEL_GUID", "")
 server = HTTPServer(("0.0.0.0", PORT), SimpleHTTPRequestHandler)
 threading.Thread(target=server.serve_forever, daemon=True).start()
 
-print("PORT OK")
-print("TOKEN OK:", bool(TOKEN))
-print("CHANNEL OK:", bool(CHANNEL_GUID))
-
 bot = Robot(token=TOKEN)
 
-print("BOT CREATED")
+bot.on_message(commands=["start"])(lambda bot, message: message.reply("✅ شما عضو کانال هستید!" if bot.check_join(CHANNEL_GUID, message.chat_id) else "❌ شما عضو کانال نیستید!\n\nابتدا در کانال @AMIRTROOLER عضو شوید و دوباره /start را بزنید."))
+
+print("BOT STARTED")
 bot.run()
